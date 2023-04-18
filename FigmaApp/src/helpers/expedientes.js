f5a681110ee08e2228dc3ab8a4f22f663c07d09b
tonyZio <95496314+tonyZio@users.noreply.github.com>
2023-04-17 18:05:39 -0700

    Doc - Helpers
    Se documento y explico lo que se hace en la carpeta Helpers con la clase expedientes, permissions y roles. Falta usuarios

--- FigmaApp/src/helpers/expedientes.js
+++ FigmaApp/src/helpers/expedientes.js
@@ -1,6 +1,15 @@
+/**
+ * Clase Expediente que representa un expediente que contiene informacion personal del paciente.
+ * @param {number} id - El identificador del expediente.
+ * @param {string} paciente - El nombre del paciente asociado al expediente.
+ */
+
+
 //get the api url from the .env file
 const API_URL = import.meta.env.VITE_API_URL;
 
+
+//Define la clase Expediente
 class Expediente {
     constructor(id, paciente) {
         this.id = id;
@@ -8,16 +17,29 @@ class Expediente {
     }
 }
 
-export const getExpedientes = () => {
-    return fetch(`${API_URL}/expedientes`)
-        .then(response => response.json())
+
+/**
+ * Obtiene todos los expedientes de la API.
+ * @returns {Promise<Array<Expediente>>} - Una promesa que devuelve un array (arreglo) de Expediente.
+ */
+
+export const getExpedientes = () => { 
+    return fetch(`${API_URL}/expedientes`)//Solicitud "fetch" para obtener lista de expedientes
+        .then(response => response.json())//Conversion de la respuesta de la solicitud a json
         .then(data => {
-            console.log(data);
-            return data.map(expediente => new Expediente(expediente.id, expediente.paciente));
+            console.log(data);//registro en consola
+            return data.map(expediente => new Expediente(expediente.id, expediente.paciente));//crear una lista de expedientes
         }).catch(error => {
-            console.log(error);
+            console.log(error);//registro de error en consola
         });
 }
+
+
+/**
+ * Usa la API para obtener un expediente por el ID.
+ * @param {number} id - El identificador del expediente a obtener.
+ * @returns {Promise<Expediente>} - Una promesa que devuelve un objeto Expediente.
+ */
 
 export const getExpediente = (id) => {
     return fetch(`${API_URL}/expedientes/${id}`)
@@ -28,6 +50,13 @@ export const getExpediente = (id) => {
             console.log(error);
         });
 }
+
+
+/**
+ * Crea un nuevo expediente en la API.
+ * @param {Expediente} expediente - El objeto Expediente a crear.
+ * @returns {Promise<Expediente>} - Una promesa que devuelve el objeto Expediente creado.
+ */
 
 export const createExpediente = (expediente) => {
     return fetch(`${API_URL}/expedientes`, {
@@ -44,13 +73,20 @@ export const createExpediente = (expediente) => {
         });
 }
 
+
+/**
+ * Actualiza un expediente existente en la API.
+ * @param {Expediente} expediente - El objeto Expediente a actualizar.
+ * @returns {Promise<Expediente>} - Una promesa que devuelve el objeto Expediente actualizado.
+ */
+
 export const updateExpediente = (expediente) => {
     return fetch(`${API_URL}/expedientes/${expediente.id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(expediente)
+        body: JSON.stringify(expediente) // Convierte el objeto Expediente a formato json y lo envía en el cuerpo de la solicitud
     }).then(response => response.json())
         .then(data => {
             return new Expediente(data.id, data.paciente);
@@ -58,6 +94,13 @@ export const updateExpediente = (expediente) => {
             console.log(error);
         });
 }
+
+
+/**
+ * Elimina un expediente existente en la API dado su identificador.
+ * @param {number} id - El identificador del expediente a eliminar.
+ * @returns {Promise<Expediente>} - Una promesa que devuelve el objeto Expediente eliminado.
+ */
 
 export const deleteExpediente = (id) => {
     return fetch(`${API_URL}/expedientes/${id}`, {
--- FigmaApp/src/helpers/roles.js
+++ FigmaApp/src/helpers/roles.js
@@ -1,12 +1,24 @@
 //get the api url from the .env file
 const API_URL = import.meta.env.VITE_API_URL;
 
+
+/**
+ * Representa un rol.
+ * @class
+ */
+
 class Role {
     constructor(id, name) {
         this.id = id;
         this.name = name;
     }
 }
+
+/**
+ * Representa un rol con sus permisos asociados.
+ * @class
+ * @extends Role
+ */
 
 class RolePermits extends Role {
     constructor(id, name, permissions) {
@@ -15,41 +27,69 @@ class RolePermits extends Role {
     }
 }
 
+
+/**
+ * Obtiene todos los roles.
+ * @function
+ * @returns {Promise<Array<Role>>} Una promesa que se resuelve en un arreglo de roles.
+ */
+
 export const getRoles = () => {
     return fetch(`${API_URL}/roles`)
-        .then(response => response.json())
+        .then(response => response.json())// Hace parse la respuesta HTTP como JSON
         .then(data => {
-            return data.map(role => new Role(role.id, role.name));
+            return data.map(role => new Role(role.id, role.name));// Crea un objeto Role por cada rol en los datos obtenidos
         }).catch(error => {
             console.log(error);
         });
 }
+
+
+/**
+ * Obtiene un rol específico.
+ * @function
+ * @param {number} id - El id del rol a obtener.
+ * @returns {Promise<RolePermits>} Una promesa que se resuelve en una instancia de RolePermits.
+ */
 
 export const getRole = (id) => {
     return fetch(`${API_URL}/roles/${id}`)
-        .then(response => response.json())
+        .then(response => response.json()) // Hace parse la respuesta HTTP como JSON
         .then(data => {
-            return new RolePermits(data.id, data.name, data.permissions);
+            return new RolePermits(data.id, data.name, data.permissions);// Crea un objeto RolePermits con los datos obtenidos
         }).catch(error => {
             console.log(error);
         });
 }
 
+/**
+ * Crea un nuevo rol.
+ * @function
+ * @param {Role} role - El objeto Role que representa el nuevo rol.
+ * @returns {Promise<Object>} Una promesa que se resuelve en el objeto JSON del nuevo rol creado.
+ */
+
 export const createRole = (role) => {
-
     console.log(role);
-
+    // Crea un objeto RolePermits con los datos obtenidos
     return fetch(`${API_URL}/roles`, {
-        method: 'POST',
+        method: 'POST', // Especifica el método HTTP POST
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json' // Convierte el objeto de rol a JSON y lo envía en el cuerpo de la solicitud
         },
-        body: JSON.stringify(role)
-    }).then(response => response.json())
+        body: JSON.stringify(role) // Convierte el objeto de rol a JSON y lo envía en el cuerpo de la solicitud
+    }).then(response => response.json()) // Hace parse la respuesta HTTP como JSON
         .catch(error => {
             console.log(error);
         });
 }
+
+/**
+ * Actualiza un rol existente.
+ * @function
+ * @param {Role} role - El objeto Role que representa el rol a actualizar.
+ * @returns {Promise<Object>} Una promesa que se resuelve en el objeto JSON del rol actualizado.
+ */
 
 export const updateRole = (role) => {
     
@@ -64,6 +104,12 @@ export const updateRole = (role) => {
                 console.log(error);
             });
     }
+
+/**
+ * Realiza una solicitud de eliminación HTTP DELETE al servidor para eliminar un rol con el ID especificado.
+ * @param {number} id - El ID del rol que se desea eliminar.
+ * @returns {Promise} Una promesa que se resuelve con los datos de la respuesta del servidor en caso de éxito, o se rechaza con el error en caso de fallo.
+ */
 
 export const deleteRole = (id) => {
     return fetch(`${API_URL}/roles/${id}`, {
